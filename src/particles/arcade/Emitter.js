dc3500706fe02e8dc680cbde3796ea5b812bd4e9
Felix Wehnert <flashspys@gmail.com>
2015-01-02 13:56:03 +0100

    fail

--- src/particles/arcade/Emitter.js
+++ src/particles/arcade/Emitter.js
@@ -126,8 +126,7 @@ Phaser.Particles.Arcade.Emitter = function (game, x, y, maxParticles) {
     this.angularDrag = 0;
 
     /**
-    * @property {number} frequency - How often a particle is emitted in 
-ms (if emitter is started with Explode === false).
+    * @property {boolean} frequency - How often a particle is emitted in ms (if emitter is started with Explode === false).
     * @default
     */
     this.frequency = 100;
